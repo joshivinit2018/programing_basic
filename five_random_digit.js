b92e4c917fc897224829454fa3d76d7ec2127ee7
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-20 20:50:05 +0530

    [vinit]:Add five digit random values adn get Addition of five random value and avrage

---
+++ five_random_digit.js
@@ -0,0 +1,23 @@
+var A1 =Math.floor(Math.random()*10);
+console.log("singal digit " +A1);
+
+var A2 =Math.floor(Math.random()*10);
+console.log("singal digit " +A2);
+
+var A3 =Math.floor(Math.random()*10);
+console.log("singal digit " +A3);
+
+var A4 =Math.floor(Math.random()*10);
+console.log("singal digit " +A4);
+
+var A5 =Math.floor(Math.random()*10);
+console.log("singal digit " +A5);
+
+var sum = A1+A2+A3+A4+A5;
+console.log("sum is" +sum);
+
+var avg = sum/5;
+console.log("avrage is "+avg);
+
+
+
