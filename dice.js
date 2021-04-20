55fbb49b8b290f24f38a5498d4d9a5b8fc732927
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-20 20:40:05 +0530

    [vinit]:add sum of two Dice

---
+++ dice.js
@@ -0,0 +1,5 @@
+var Dice1= Math.floor(Math.random()*6 + 1);
+console.log("Dice 1:" +Dice1);
+
+var Dice2= Math.floor(Math.random()*6 + 1);
+console.log("Dice 2:" +Dice2);
---
+++ twodice.js
@@ -0,0 +1,9 @@
+var Dice1= Math.floor(Math.random()*6 + 1);
+console.log("Dice 1:" +Dice1);
+
+var Dice2= Math.floor(Math.random()*6 + 1);
+console.log("Dice 2:" +Dice2);
+
+var sum = Dice1 + Dice2;
+
+console.log("sum of two dice is " +sum);
